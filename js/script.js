9b5dcfcaf98c37f4bd12f1876b877a0f9b66b47c
Benedettopana <benedetto.pana@gmail.com>
2024-02-29 17:39:22 +0100

    [add]bonus

--- js/script.js
+++ js/script.js
@@ -1,18 +1,49 @@
-
 const gridContainer = document.querySelector('.grid-container');
+// Selezione difficoltà
+const selezione = document.querySelector('.difficulty');
+const difficulty = selezione.value;
+// btn start
+const btnPlay = document.querySelector('.btn-play');
+
 // RESET
 reset();
 
-for(let i= 0; i<100; i++){
-  // console.log(i);
-  const square = getSquare(i);
-  // console.log('ciao');
-  gridContainer.append(square);
 
-  console.log(square);
+btnPlay.addEventListener('click', startPlay(difficulty));
+
+function startPlay(difficulty){
   
-}
-
+  reset();
+  let nSquare;
+  
+  if(difficulty == 0){
+    nSquare = 100;
+  
+  }else if(difficulty == 1){
+    nSquare = 81;
+    
+  }else{
+    nSquare = 49;
+    
+  }
+  
+  
+  
+  for(let i= 0; i<nSquare; i++){
+    // console.log(i);
+    const square = getSquare(i);
+    if(difficulty == 1){
+      square.classList.add('dif-medium');
+    }else if(difficulty == 2){
+      square.classList.add('dif-hard');
+    }
+    
+    gridContainer.append(square);
+  
+    console.log(square);
+    
+  }
+  }
 
 
 
@@ -29,6 +60,8 @@ for(let i= 0; i<100; i++){
 //                         FUNZIONI
 /////////////////////////////////////////////////////////////////////////////////////
 
+
+
 function reset(){
   console.log(this);
   gridContainer.innerHTML = '';
@@ -38,13 +71,12 @@ function reset(){
 function getSquare(numero){
   const sq = document.createElement('div');
   sq.className = 'square';
-
+  
   // Proprietà custom
   // console.log(numero);
   sq._sqID = numero;
   const porcodio = this._sqID;
 
-  console.log(this);
   sq.addEventListener('click', function(){
     const numero = this._sqID;
     
